13c3f96905faeb119cc11729271156aaed46bbf9
Sotaneum <gnyontu39@gmail.com>
2021-05-23 16:24:22 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import { createStore } from "redux";
+
+const counter = document.querySelector("h1");
+const divToggle = document.querySelector(".toggle");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+
+const decrease = () => ({ type: DECREASE });
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+
+const initialState = {
+  counter: 0,
+  toggle: false,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
